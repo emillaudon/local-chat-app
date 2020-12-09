b9ed8545a2be98dcd903fd1ab39ff914cab683fc
emillaudon <emil.laudn@gmail.com>
2020-12-09 12:53:56 +0100

    handling for empty messages

--- app/src/main/java/com/example/myapplication/PostActivity.kt
+++ app/src/main/java/com/example/myapplication/PostActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
@@ -14,14 +15,19 @@ class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+        title = "Create Post"
 
         postEditText = findViewById(R.id.editTextPost)
 
         val postButton = findViewById<Button>(R.id.postbutton)
 
         postButton.setOnClickListener {
-            println("ff")
-            finishedPost()
+            if(postEditText.text != null && postEditText.text.length > 1) {
+                finishedPost()
+            } else {
+                Toast.makeText(baseContext, "You have to type something.", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
