0c0d1799313a8d2fd1dae13d3f8d2d45ebba5ffc
Johannes Bjurströmer <55398759+johannesbjur@users.noreply.github.com>
2020-12-19 10:45:34 +0100

    removed hard coded location

--- app/src/main/java/com/example/myapplication/models/TemperatureHandler.kt
+++ app/src/main/java/com/example/myapplication/models/TemperatureHandler.kt
@@ -82,11 +82,8 @@ class TemperatureHandler(private val activity: Activity) {
 
             val thread = Thread(Runnable {
                 try {
-//                    val long = location.longitude.toString().take(9)
-//                    val lat = location.latitude.toString().take(9)
-
-                    val long = 18.063240.toString()
-                    val lat = 59.334591.toString()
+                    val long = location.longitude.toString().take(9)
+                    val lat = location.latitude.toString().take(9)
 
 
                     val apiUrl = URL("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" +
